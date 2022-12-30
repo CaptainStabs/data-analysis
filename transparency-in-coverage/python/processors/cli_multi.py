7005c955f14c4f30eceac73c422e69116289bc71
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-12-30 16:01:23 -0500

    Create cli_multi.py

---
+++ transparency-in-coverage/python/processors/cli_multi.py
@@ -0,0 +1,56 @@
+import multiprocessing
+import pandas as pd
+from mrfutils import import_csv_to_set, json_mrf_to_csv
+from multiprocessing import Pool, Manager
+import sys
+import traceback as tb
+import argparse
+from tqdm import tqdm
+from _utils import istarmap
+
+
+
+def master_function(url, out):
+    id = multiprocessing.current_process()._identity[0] + 1
+    try:
+        json_mrf_to_csv(loc=str(url), url=str(url), npi_filter=import_csv_to_set("quest/npis.csv"), code_filter= import_csv_to_set("quest/codes.csv"), out_dir=out, pos=id)
+
+    except:
+        print(url, "\n", id)
+        tb.print_exc()
+
+
+def apply_parallel(df, func, out):
+    try:
+        with multiprocessing.Pool(processes=16) as pool:
+            for _ in tqdm(pool.istarmap(func, [(row["url"], out) for _, row in df.iterrows()]), desc="Main Process", position=0, total=len(df)):
+                pass
+            pool.close()
+    except KeyboardInterrupt:
+        print("Quitting")
+        pool.terminate()
+        sys.exit()
+    
+    except Exception:
+        pool.terminate()
+        tb.print_exc()
+        raise
+
+    finally:
+        pool.join()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', default="_input_csvs/first_100.csv")
+    parser.add_argument('-o', '--out', default = 'test_100')
+    args = parser.parse_args()
+    out = args.out
+
+    try:
+        df = pd.read_csv(args.input)
+        df = df.sort_values(by=["size"])
+        results = apply_parallel(df, master_function, out)
+    except KeyboardInterrupt:
+        print("Quitting")
+        sys.exit()
